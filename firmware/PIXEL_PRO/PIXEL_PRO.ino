04f39e28e742b1adba5c8de037725315218572e9
mihqag <85729642+mihqag148@users.noreply.github.com>
2026-09-24 02:23:01 +0700

    Bump PIXEL PRO firmware to 1.8.8

--- firmware/PIXEL_PRO/PIXEL_PRO.ino
+++ firmware/PIXEL_PRO/PIXEL_PRO.ino
@@ -17,7 +17,7 @@
 USBCDC USBSerial;
 #endif
 
-static constexpr char FW_VERSION[] = "1.8.7";
+static constexpr char FW_VERSION[] = "1.8.8";
 static constexpr uint16_t USB_VID_PIXEL = 0x303A;
 static constexpr uint16_t USB_PID_PIXEL = 0x80C2;
 static constexpr uint8_t KEY_COUNT = 8;
@@ -7927,7 +7927,7 @@ void setup() {
   USB.productName("PIXEL PRO");
   USB.manufacturerName("Lumi3D");
   USB.serialNumber(serial);
-  USB.firmwareVersion(0x0187);
+  USB.firmwareVersion(0x0188);
 
   // Normal Lumi Macropad CDC traffic must never be interpreted as a request
   // to enter the ESP32-S2 bootloader. Firmware updates use the dedicated ROM
@@ -7941,7 +7941,7 @@ void setup() {
 
   delay(500);
   sendMappedReports();
-  cdcPrintln("BOOT|PIXELPRO|1.8.7");
+  cdcPrintln("BOOT|PIXELPRO|1.8.8");
 }
 
 void loop() {
